ee9c10dd408528a53f40a25de2ca94cc86678d30
Kevin Nguyen <60084108+Kvnvn@users.noreply.github.com>
2020-03-23 02:28:42 -0700

    added in dump for 5day forecast and rendering history

--- script.js
+++ script.js
@@ -44,6 +44,65 @@ function dumpUvData(){
         currentWeatherView.text(JSON.stringify(response, null, 2));
     });  
 }
+function dump5DayForecastData(){
+    var location = {lon: -118.97, lat: 37.65};
+    var queryURL=
+    "https://api.openweathermap.org/data/2.5/forecast?" +
+     "q=" + 
+     searchCities +
+    "&" + 
+    apiKeyParam;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        currentWeatherView.text(JSON.stringify(response, null, 2));
+        
+        
+        var offsetSeconds =response.city.timezone;
+        var FiveDayForecastData =[];
+
+        response.list.forEach(function(forecastListItem){
+        var seconds= forecastListItem.dt + offsetSeconds
+        var hours = seconds/3600
+        var hour = hours % 24
+        if (hour>12 && hour <=15)
+        var forecastData={
+            unixTime:forecastListItem.dt,
+            temperature: forecastListItem.main.temp,
+            humidityPercent:forecastListItem.main.humidity,
+            iconUrl: getWeatherIconUrlFromId(forecastListItem.weather[0].icon)
+        };
+        FiveDayForecastData.push(forecastData)
+        });
+    });  
+    
+}
+//icon for weather id 
+function getWeatherIconUrlFromId(iconId){
+    return "http://openweathermap.org/img/wn" +iconId + "@2x.png"
+}
+//rendering history 
+function renderHistory(searchHistory){
+
+var newListGroup = $("<div>")
+
+searchHistory.forEach(function(city){
+
+var button = $("<button>").attr({
+type:'button',
+class:'list-group-item list-group-item-action'
+}).text(city)
+newListGroup.append(button)
+})
+
+$(".list-group").empty()
+.append(newListGroup);
+
+var searchHistory =["Austin","Chicago","New York","Orlando","San Francisco","Seattle","Denver","Atlanta"]
+renderHistory(searchHistory);
+}
 
 
 //dumpUvData();
