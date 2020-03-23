d9b0a4de0ffb7f18013836e571ea33484d5eb02b
Kevin Nguyen <60084108+Kvnvn@users.noreply.github.com>
2020-03-23 02:26:30 -0700

    addded in dumpUvdata

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ var searchCities = "Mammoth Lakes";
 var apiKeyParam = "appid=0c18dc71f2179d55a217164f2d1005e6";
 var unitsFormat = "units-imperial";
 
-
+//when dumpcurrent weather happens it should take api and store it 
+//current weather data should correspond to things we are looking for
 function dumpCurrentWeather() {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Mammoth Lakes&" + unitsFormat + "&" + apiKeyParam;
    
@@ -25,7 +26,8 @@ function dumpCurrentWeather() {
         console.log(currentWeatherData)
     });
 }
-
+//in dump data it should take api source and place in storage 
+//use JSON stringify to "pretty print"
 function dumpUvData(){
     var location = {lon: -118.97, lat: 37.65};
     var queryURL=
@@ -42,67 +44,8 @@ function dumpUvData(){
         currentWeatherView.text(JSON.stringify(response, null, 2));
     });  
 }
-function dump5DayForecastData(){
-    var location = {lon: -118.97, lat: 37.65};
-    var queryURL=
-    "https://api.openweathermap.org/data/2.5/forecast?" +
-     "q=" + 
-     searchCities +
-    "&" + 
-    apiKeyParam;
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        currentWeatherView.text(JSON.stringify(response, null, 2));
-        
-        
-        var offsetSeconds =response.city.timezone;
-        var FiveDayForecastData =[];
-
-        response.list.forEach(function(forecastListItem){
-        var seconds= forecastListItem.dt + offsetSeconds
-        var hours = seconds/3600
-        var hour = hours % 24
-        if (hour>12 && hour <=15)
-        var forecastData={
-            unixTime:forecastListItem.dt,
-            temperature: forecastListItem.main.temp,
-            humidityPercent:forecastListItem.main.humidity,
-            iconUrl: getWeatherIconUrlFromId(forecastListItem.weather[0].icon)
-        };
-        FiveDayForecastData.push(forecastData)
-        });
-    });  
-    
-}
-
-function getWeatherIconUrlFromId(iconId){
-    return "http://openweathermap.org/img/wn" +iconId + "@2x.png"
-}
-
-function renderHistory(searchHistory){
-
-var newListGroup = $("<div>")
-
-searchHistory.forEach(function(city){
-
-var button = $("<button>").attr({
-type:'button',
-class:'list-group-item list-group-item-action'
-}).text(city)
-newListGroup.append(button)
-})
-
-$(".list-group").empty()
-.append(newListGroup);
-
-var searchHistory =["Austin","Chicago","New York","Orlando","San Francisco","Seattle","Denver","Atlanta"]
-renderHistory(searchHistory);
-}
 
 
 //dumpUvData();
 //dumpCurrentWeather();
-dump5DayForecastData();
+//dump5DayForecastData();
