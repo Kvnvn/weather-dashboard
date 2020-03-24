6710f2d7d242eda44553fdc315100246104e76cc
Kevin Nguyen <60084108+Kvnvn@users.noreply.github.com>
2020-03-24 00:29:36 -0700

    fixed current weather card to display user input through search button

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
 
-var currentWeatherView = $("#currentWeather");
-var searchCities = $("#searchUserInput").val().trim();
+// var currentWeatherView = $("#currentWeather");
 var apiKeyParam = "appid=0c18dc71f2179d55a217164f2d1005e6";
-var unitsFormat = "units-imperial";
+
 
 //when dumpcurrent weather happens it should take api and store it 
 //current weather data should correspond to things we are looking for
-function dumpCurrentWeather() {
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCities + unitsFormat + "&" + apiKeyParam;
-   
+function dumpCurrentWeather(cities) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cities + "&" + apiKeyParam;
+    
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        currentWeatherView.text(JSON.stringify(response, null, 2));
         var currentWeatherData = {
             cityName: response.name,
             unixTime: response.dt,
-            iconId: getWeatherIconFromId(response.weather[0].icon),
+            // iconId: getWeatherIconFromId(response.weather[0].icon),
             temperature: response.main.temp,
             humidityPercent: response.main.humidity,
-            windSpeedMetersSec: response.wind.speed,
+            windSpeed: response.wind.speed,
             Location:response.coord,
         };
-        console.log(currentWeatherData)
+            console.log(currentWeatherData);
+
+            var nameOfCity = $("<h2>").text(currentWeatherData.cityName);
+            var currentTemp = $("<p>").text("Temperature: " + (((currentWeatherData.temperature- 273.15) *(9 / 5 ) + 32 ).toFixed(2)) + " F");
+            var currentHumidity = $("<p>").text("Humidity: " + currentWeatherData.humidityPercent);
+            var currentWindSpeed= $("<p>").text("Wind Speed: " + currentWeatherData.windSpeed);
+            //var uvIn = $("<h2>").text(currentWeatherData.);
+
+            $("#weather-body").empty();
+            $("#weather-body").append(nameOfCity,currentTemp,currentHumidity,currentWindSpeed);
     });
-// }
+}
 // //in dump data it should take api source and place in storage 
 // //use JSON stringify to "pretty print"
-// function dumpUvData(){
-    
-//     var location = {lon: response.lon, lat: response.lat};
+// function dumpUvData(response){
 //     var queryURL=
 //     "https://api.openweathermap.org/data/2.5/uvi?lat="+ 
-//     location.lat + 
+//     response.lat + 
 //     "&lon=" +
 //     location.lon +
 //     "&" + apiKeyParam;
@@ -45,6 +50,7 @@ function dumpCurrentWeather() {
 //         currentWeatherView.text(JSON.stringify(response, null, 2));
 //     });  
 // }
+
 // function dump5DayForecastData(){
 //     // var location = {lon: -118.97, lat: 37.65};
 //     var queryURL=
@@ -80,32 +86,21 @@ function dumpCurrentWeather() {
 //     });  
     
 // }
-// //icon for weather id 
+
+//icon for weather id 
 // function getWeatherIconUrlFromId(iconId){
-//     return "http://openweathermap.org/img/wn" +iconId + "@2x.png"
+//     return "http://openweathermap.org/img/wn" + iconId + "@2x.png";
 // }
-// //rendering history 
-// function renderHistory(searchHistory){
-
-// var newListGroup = $("<div>")
-
-// searchHistory.forEach(function(city){
-
-// var button = $("<button>").attr({
-// type:'button',
-// class:'list-group-item list-group-item-action'
-// }).text(city)
-// newListGroup.append(button)
-// })
-
-// $(".list-group").empty()
-// .append(newListGroup);
-
-// var searchHistory =[]
-// renderHistory(searchHistory);
-// }
-
 
 // dumpUvData();
 // dumpCurrentWeather();
 // dump5DayForecastData();
+
+
+// when user clicks on the search button
+$("#searchBtn").on("click", function (event) {
+    event.preventDefault();
+    var searchCities = $("#searchUserInput").val().trim();
+    
+    dumpCurrentWeather(searchCities);
+});
