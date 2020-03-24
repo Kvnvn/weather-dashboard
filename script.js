7212f72e3a0f602a8e53ab0a70b475ab77c37a46
Kevin Nguyen <60084108+Kvnvn@users.noreply.github.com>
2020-03-23 23:12:51 -0700

    cleaned up html and well as rows and columns

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 var currentWeatherView = $("#currentWeather");
-var searchCities = "Mammoth Lakes";
+var searchCities = $("#searchUserInput").val().trim();
 var apiKeyParam = "appid=0c18dc71f2179d55a217164f2d1005e6";
 var unitsFormat = "units-imperial";
 
 //when dumpcurrent weather happens it should take api and store it 
 //current weather data should correspond to things we are looking for
 function dumpCurrentWeather() {
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Mammoth Lakes&" + unitsFormat + "&" + apiKeyParam;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCities + unitsFormat + "&" + apiKeyParam;
    
     $.ajax({
         url: queryURL,
@@ -25,86 +25,87 @@ function dumpCurrentWeather() {
         };
         console.log(currentWeatherData)
     });
-}
-//in dump data it should take api source and place in storage 
-//use JSON stringify to "pretty print"
-function dumpUvData(){
-    var location = {lon: -118.97, lat: 37.65};
-    var queryURL=
-    "https://api.openweathermap.org/data/2.5/uvi?lat="+ 
-    location.lat + 
-    "&lon=" +
-    location.lon +
-    "&" + apiKeyParam;
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        currentWeatherView.text(JSON.stringify(response, null, 2));
-    });  
-}
-function dump5DayForecastData(){
-    var location = {lon: -118.97, lat: 37.65};
-    var queryURL=
-    "https://api.openweathermap.org/data/2.5/forecast?" +
-     "q=" + 
-     searchCities +
-    "&" + 
-    apiKeyParam;
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        currentWeatherView.text(JSON.stringify(response, null, 2));
-        
-        
-        var offsetSeconds =response.city.timezone;
-        var FiveDayForecastData =[];
-
-        response.list.forEach(function(forecastListItem){
-        var seconds= forecastListItem.dt + offsetSeconds
-        var hours = seconds/3600
-        var hour = hours % 24
-        if (hour>12 && hour <=15)
-        var forecastData={
-            unixTime:forecastListItem.dt,
-            temperature: forecastListItem.main.temp,
-            humidityPercent:forecastListItem.main.humidity,
-            iconUrl: getWeatherIconUrlFromId(forecastListItem.weather[0].icon)
-        };
-        FiveDayForecastData.push(forecastData)
-        });
-    });  
+// }
+// //in dump data it should take api source and place in storage 
+// //use JSON stringify to "pretty print"
+// function dumpUvData(){
     
-}
-//icon for weather id 
-function getWeatherIconUrlFromId(iconId){
-    return "http://openweathermap.org/img/wn" +iconId + "@2x.png"
-}
-//rendering history 
-function renderHistory(searchHistory){
+//     var location = {lon: response.lon, lat: response.lat};
+//     var queryURL=
+//     "https://api.openweathermap.org/data/2.5/uvi?lat="+ 
+//     location.lat + 
+//     "&lon=" +
+//     location.lon +
+//     "&" + apiKeyParam;
 
-var newListGroup = $("<div>")
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response) {
+//         currentWeatherView.text(JSON.stringify(response, null, 2));
+//     });  
+// }
+// function dump5DayForecastData(){
+//     // var location = {lon: -118.97, lat: 37.65};
+//     var queryURL=
+//     "https://api.openweathermap.org/data/2.5/forecast?" +
+//      "q=" + 
+//      searchCities +
+//     "&" + 
+//     apiKeyParam;
 
-searchHistory.forEach(function(city){
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response) {
+//         currentWeatherView.text(JSON.stringify(response, null, 2));
+        
+        
+//         var offsetSeconds = response.city.timezone;
+//         var FiveDayForecastData =[];
 
-var button = $("<button>").attr({
-type:'button',
-class:'list-group-item list-group-item-action'
-}).text(city)
-newListGroup.append(button)
-})
+//         response.list.forEach(function(forecastListItem){
+//         var seconds= forecastListItem.dt + offsetSeconds
+//         var hours = seconds/3600
+//         var hour = hours % 24
+//         if (hour>12 && hour <=15)
+//         var forecastData={
+//             unixTime:forecastListItem.dt,
+//             temperature: forecastListItem.main.temp,
+//             humidityPercent:forecastListItem.main.humidity,
+//             iconUrl: getWeatherIconUrlFromId(forecastListItem.weather[0].icon)
+//         };
+//         FiveDayForecastData.push(forecastData)
+//         });
+//     });  
+    
+// }
+// //icon for weather id 
+// function getWeatherIconUrlFromId(iconId){
+//     return "http://openweathermap.org/img/wn" +iconId + "@2x.png"
+// }
+// //rendering history 
+// function renderHistory(searchHistory){
 
-$(".list-group").empty()
-.append(newListGroup);
+// var newListGroup = $("<div>")
 
-var searchHistory =["Austin","Chicago","New York","Orlando","San Francisco","Seattle","Denver","Atlanta"]
-renderHistory(searchHistory);
-}
+// searchHistory.forEach(function(city){
+
+// var button = $("<button>").attr({
+// type:'button',
+// class:'list-group-item list-group-item-action'
+// }).text(city)
+// newListGroup.append(button)
+// })
+
+// $(".list-group").empty()
+// .append(newListGroup);
+
+// var searchHistory =[]
+// renderHistory(searchHistory);
+// }
 
 
-//dumpUvData();
-//dumpCurrentWeather();
-//dump5DayForecastData();
+// dumpUvData();
+// dumpCurrentWeather();
+// dump5DayForecastData();
